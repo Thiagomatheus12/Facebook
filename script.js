de2471fa507f2a76941df8993b6c957b6f9fd844
Thiagomatheus12 <thi_ago5@hotmail.com>
2022-05-18 12:44:47 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+function toogleStyleBoxEmail() {
+    var email = document.getElementById("email");
+    var divErro = document.getElementById("erro");
+    var divCorreto = document.getElementById("correto");
+    if (validateEmail(email.value)) {
+        email.classList.remove("erro")
+        divErro.classList.remove("d-block") 
+        email.classList.add("correto")
+        divCorreto.classList.add("d-blockcorreto")
+    } else {
+        email.classList.add("erro")
+        divErro.classList.add("d-block")
+        email.classList.remove("correto")
+        divCorreto.classList.remove("d-blockcorreto")
+        alert("Digite um E-mail válido")
+    }
+    console.log(email.value)
+}
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function verificarSenha() {
+    var senha = document.getElementById("senha");
+    console.log(senha.value)
+}
+
+
+
+function anoAtual() {
+    const ano = new Date().getFullYear()
+    document.getElementById("ano").innerHTML = ano
+}
+
+
+
